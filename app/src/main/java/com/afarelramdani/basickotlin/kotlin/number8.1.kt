afde7b97e3e4e651b91dc869e441d6ca1d612b5c
afarelramdani <ahmadfarhanel@gmail.com>
2020-11-30 14:01:23 +0700

    When Expression

--- app/src/main/java/com/afarelramdani/basickotlin/kotlin/number8.1.kt
+++ app/src/main/java/com/afarelramdani/basickotlin/kotlin/number8.1.kt
@@ -23,11 +23,23 @@ fun hasil(mtk: Int?, indo: Int?, inggris: Int?, ipa: Int?){
         print("Data Tidak Valid")
     } else {
         var grade = (mtk!! + indo!! + inggris!! + ipa!!)/4
-        if(grade > 90 ) println("A")
-        else if(grade > 80 ) println("B")
-        else if(grade > 70 ) println("C")
-        else if(grade > 60 ) println("D")
-        else  println("E")
+        
+//        if(grade > 90-100 ) println("A")
+//        else if(grade > 80 ) println("B")
+//        else if(grade > 70 ) println("C")
+//        else if(grade > 60 ) println("D")
+//        else  println("E")
+
+        when (grade) {
+            in 90..100 -> print("A")
+            in 80..89 -> print("B")
+            in 70..79 -> print("C")
+            in 60..69 -> print("D")
+            in 0..59 -> print("E")
+            else -> print("Data Tidak Valid")
+
+        }
+
 
     }
 
